b6f2828e86650d5aa15fea924a3d397604d07116
Bruno Melo <bruno.melo@idor.org>
2017-03-07 18:42:54 -0300

    Treating some exceptions - trying to correct small mistakes with size data.

--- src/calcRMS_por_bloco.m
+++ src/calcRMS_por_bloco.m
@@ -3,16 +3,31 @@ function [rmsRepouso rmsActivation p] = calcRMS_por_bloco( data, inds_repouso_bl
 rmsRepouso = zeros(length(inds_repouso_bloco),1);
 rmsActivation = zeros(length(inds_activation_bloco),1);
 
-for b=1:length(inds_repouso_bloco)
-    
-    rmsRepouso(b) = calcRMS( data(inds_repouso_bloco{b}) );
-    
+% Alerting about possible error
+data_len = length(data);
+fix = false;
+if  data_len > 19000 && any([inds_repouso_bloco{:} inds_activation_bloco{:}] > data_len)
+    fix = true;
+    warning( sprintf('Data with fewer elements (%d).', data_len), 'rms:datasize');
 end
 
-for b=1:length(inds_repouso_bloco)
 
-    rmsActivation(b) = calcRMS( data(inds_activation_bloco{b} ) );
+for b=1:length(inds_repouso_bloco)
+    % Fixing size
+    if fix
+        inds_repouso_bloco{b}(inds_repouso_bloco{b} > data_len) = [];
+    end
     
+    rmsRepouso(b) = calcRMS( data(inds_repouso_bloco{b}) );
+end
+
+for b=1:length(inds_activation_bloco)   
+    % Fixing size
+    if fix
+        inds_activation_bloco{b}(inds_activation_bloco{b} > data_len) = [];
+    end
+    
+    rmsActivation(b) = calcRMS( data(inds_activation_bloco{b} ) );
 end
     
 [h p] = ttest2( rmsRepouso, rmsActivation, 0.05, 'both' );
