456e021c92d02fe021363f05913d045e1b87feae
Bruno Melo <bruno.melo@idor.org>
2017-03-08 09:39:32 -0300

    Change in the logic of error data size.

--- src/calcRMS_por_bloco.m
+++ src/calcRMS_por_bloco.m
@@ -3,29 +3,27 @@ function [rmsRepouso rmsActivation p] = calcRMS_por_bloco( data, inds_repouso_bl
 rmsRepouso = zeros(length(inds_repouso_bloco),1);
 rmsActivation = zeros(length(inds_activation_bloco),1);
 
-% Alerting about possible error
+% Checking problems with data
 data_len = length(data);
-fix = false;
-if  data_len > 19000 && any([inds_repouso_bloco{:} inds_activation_bloco{:}] > data_len)
-    fix = true;
+all_inds = [inds_repouso_bloco{:} inds_activation_bloco{:}];
+thr = round(max(all_inds)*0.95);
+if data_len < thr
+    error('Corrupted data.');
+elseif  any(all_inds > data_len)
     warning( sprintf('Data with fewer elements (%d).', data_len), 'rms:datasize');
 end
 
 
 for b=1:length(inds_repouso_bloco)
     % Fixing size
-    if fix
-        inds_repouso_bloco{b}(inds_repouso_bloco{b} > data_len) = [];
-    end
+    inds_repouso_bloco{b}(inds_repouso_bloco{b} > data_len) = [];
     
     rmsRepouso(b) = calcRMS( data(inds_repouso_bloco{b}) );
 end
 
 for b=1:length(inds_activation_bloco)   
     % Fixing size
-    if fix
-        inds_activation_bloco{b}(inds_activation_bloco{b} > data_len) = [];
-    end
+    inds_activation_bloco{b}(inds_activation_bloco{b} > data_len) = [];
     
     rmsActivation(b) = calcRMS( data(inds_activation_bloco{b} ) );
 end
